c0ec049b8d2b2499f24b472dd7ca50449aba7584
Peter Hessey <peter.hessey@hotmail.com>
2025-06-08 15:32:44 +0100

    Try making border

--- ratatui-tutorials/move-the-cube/src/ui.rs
+++ ratatui-tutorials/move-the-cube/src/ui.rs
@@ -1,26 +1,43 @@
 use crate::app::{App, Coordinate};
 use ratatui::Frame;
 use ratatui::layout::{Constraint, Flex, Layout, Position, Rect};
-use ratatui::style::Color;
+use ratatui::style::{Color, Style, Stylize};
+use ratatui::widgets::{Block, Borders};
 
 pub fn ui(frame: &mut Frame, app: &App) {
-    let text_area = center(
+    let buf = frame.buffer_mut();
+    let border_area = center(
         frame.area(),
+        Constraint::Length(app.world_size * 2 + 2),
+        Constraint::Length(app.world_size + 2),
+    );
+    for (idy, y) in (border_area.top()..border_area.bottom()).enumerate() {
+        for (idx, x) in (border_area.left()..border_area.right()).enumerate() {
+            if idx == 0 || idy == y as usize {
+                buf[Position::new(x, y)]
+                    .set_char('█')
+                    .set_fg(Color::LightBlue);
+            }
+        }
+    }
+
+    let game_area = center(
+        border_area,
         Constraint::Length(app.world_size * 2),
         Constraint::Length(app.world_size),
     );
-    let buf = frame.buffer_mut();
-    for (idy, y) in (text_area.top()..text_area.bottom()).enumerate() {
-        for (idx, x) in (text_area.left()..text_area.right()).enumerate() {
-            let mut fg: Color = Color::Yellow;
+
+    for (idy, y) in (game_area.top()..game_area.bottom()).enumerate() {
+        for (idx, x) in (game_area.left()..game_area.right()).enumerate() {
             let coordinates_to_eval = Coordinate {
                 x: idx as u16 / 2,
                 y: idy as u16,
             };
             if coordinates_to_eval == app.cube_location {
-                fg = Color::Black;
+                buf[Position::new(x, y)]
+                    .set_char('█')
+                    .set_fg(Color::LightBlue);
             }
-            buf[Position::new(x, y)].set_char('█').set_fg(fg);
         }
     }
 }
