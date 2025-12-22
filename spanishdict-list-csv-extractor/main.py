85b01ac6eef1128e91c76bb2dbda4c7a20dc4344
Peter Hessey <45258028+peterhessey@users.noreply.github.com>
2025-12-22 16:08:58 +0000

    Make spanishdict list extractor (#2)
    * Set up initial files
    
    * Add deps
    
    * Create first functional script
    
    * Click-ify the script
    
    * Add custom gitignore
    
    * Update README
    
    * Update param names

---
+++ spanishdict-list-csv-extractor/main.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import click
+
+SPANISH_DICT_LISTS_URL = "https://www.spanishdict.com/lists/"
+TRANSLATION_PAIR_DIV_ID = "M51vAoht"
+SPANISH_WORD_DIV_ID = "UO6pWUJR"
+ENGLISH_WORLD_DIV_ID = "xLusdC9B"
+
+
+@click.command()
+@click.argument(
+    "list_id",
+)
+@click.option(
+    "--output", "-o", default="spanish_translations.csv", help="Output CSV filename"
+)
+def main(
+    list_id: str,
+    output: str,
+) -> None:
+    url = SPANISH_DICT_LISTS_URL + list_id
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    full_response_html = BeautifulSoup(response.text, "html.parser")
+
+    translation_pair_divs = full_response_html.find_all(
+        "div", class_=TRANSLATION_PAIR_DIV_ID
+    )
+
+    with open(output, "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile)
+        for div in translation_pair_divs:
+            spanish_div = div.find("div", class_=SPANISH_WORD_DIV_ID)
+            english_div = div.find("div", class_=ENGLISH_WORLD_DIV_ID)
+
+            if spanish_div and english_div:
+                spanish_text = spanish_div.get_text(strip=True)
+                english_text = english_div.get_text(strip=True)
+                writer.writerow([spanish_text, english_text])
+            else:
+                print(
+                    f"WARNING: {spanish_div=}, {english_div=}. Expected contents for both. Skipping."
+                )
+
+
+if __name__ == "__main__":
+    main()
+
